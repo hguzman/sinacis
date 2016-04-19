8e9dfc137129f04afcef71f03240fb6b54fefc75
nurysmartinez <nurysmartinez2014@hotmail.com>
2016-04-19 11:49:37 -0500

    Added files via upload

---
+++ base de datos almacentallerdj.sql
@@ -0,0 +1,8 @@
+create database almacentallerdj;
+use almacentallerdj;
+create table vehiculos(
+placa varchar (6),
+marca varchar (6),
+descripcion varchar (50),
+motor varchar( 20));
+describe vehiculos;
