41049f32ad89807e8e7e7fd2145fac60e61f4f3d
nurysmartinez <nurysmartinez2014@hotmail.com>
2016-04-22 12:07:05 -0500

    Update base de datos almacentallerdj.sql

--- base de datos almacentallerdj.sql
+++ base de datos almacentallerdj.sql
@@ -1,8 +1,11 @@
 create database almacentallerdj;
 use almacentallerdj;
-create table vehiculos(
+create table clientes(
+nombre varchar(30),
+cedula bigint(20),
+telefono bigint(10),
 placa varchar (6),
 marca varchar (6),
 descripcion varchar (50),
-motor varchar( 20));
+color varchar( 20));
 describe vehiculos;
