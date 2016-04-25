074c02944f43118c825edcf585144683132eaee5
Henry Miguel Guzmán Escorcia <hguzman10@gmail.com>
2016-04-25 10:57:54 -0500

    Cambios en el script y en el menu

--- base de datos almacentallerdj.sql
+++ base de datos almacentallerdj.sql
@@ -1,11 +1,15 @@
 create database almacentallerdj;
+
 use almacentallerdj;
+
 create table clientes(
-nombre varchar(30),
-cedula bigint(20),
-telefono bigint(10),
-placa varchar (6),
-marca varchar (6),
-descripcion varchar (50),
-color varchar( 20));
+  cedula bigint,
+  nombre varchar(30),
+  telefono bigint,
+  placa varchar(6),
+  marca varchar(6),
+  descripcion varchar(50),
+  color varchar(20),
+  primary key(cedula));
+
 describe vehiculos;
---
+++ index.php
@@ -0,0 +1,54 @@
+<html>
+  <head>
+    <title>ALMACENTALLERDJ</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <script type="text/javascript" src="js/jquery-2.2.3.js"></script>    
+    <script type="text/javascript" src="js/bootstrap.js"></script>    
+  </head>
+  <body>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <?php include 'menu.php' ?>
+        </div>
+        <div class="col-md-4">
+          <h1>ALMACEN TALLER DJ</h1>
+          <hr>
+          <img src="LOGODJ.jpg" alt="" class="img-responsive">
+                 
+        </div>
+        <div class="col-md-4">
+          <h1>Bienvenidos a nuestro portal</h1>
+          <p>
+            Es un placer darles la bienvenida a la pagina de internet de Almacen y taller dj
+
+            
+            En un mundo cada vez mas digitalizado, estamos convencidos de que, a traves de este sitio, puedan conocer las actividades que realizamos en areas tan diversas como el comercio,el mantenimiento de motos, entre otras.
+
+            
+          </p>
+          <p>
+            Espero que nuestra pagina web sirva de ventana y plataforma util para ustedes. Al mismo tiempo, podran adquirir la multifacetica informacion acerca de nuestra region, nuestra cultura y nuestro pueblo, junto a variadas actividades.
+          </p>
+        </div>
+        <div class="col-md-4">
+          <div class="well">
+           MISION
+           Nuestra empresa esta dedicada a la reparacion y venta de repuestos de excelente calidad  cubriendo las necesidades de nuestro mercado para obtener plena satisfaccion de los clientes mediante la integracion de un equipo de trabajo capacitado con la maxima seguridad y calidad en nuestros servicios para su desarrollo integral.
+
+          </div>
+          <hr>
+          <div class="well">
+            VISION
+            Nos proyectamos a mediano plazo lograr ser reconocido como el mejor taller de venta de repuestos con un servicio y atencion de excelente categoria lideres  en innovacion en mantenimiento , logrando a traves de un trabajo en equipo , con una planificacion y organizacion adecuada que responda a cualquier reto del consumidor y de la competencia. 
+
+          </div>
+          
+        </div>
+      </div>
+    </div>
+    <script>
+        $('.dropdown-toggle').dropdown();
+    </script>
+  </body>
+</html>
